e26f4d04c4cbcd4c3ebd28e15768258f447ad68c
ilhamwibowo <ilhamprasetyowb@gmail.com>
2022-06-02 21:02:44 +0700

    add snake-tail collision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from turtle import Turtle, Screen
 import time
-import turtle
 from Scoreboard import Scoreboard
 from Snake import Snake
 from Food import Food
@@ -10,6 +9,7 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snaking")
+screen.tracer(0)
 
 #instances
 snake = Snake()
@@ -26,13 +26,14 @@ screen.onkey(snake.right,"Right")
 game_on = True
 while game_on:
     screen.update()
-    # time.sleep(0.1)
-    screen.delay(1)
+    time.sleep(0.1)
+    # screen.delay(1)
     snake.move()
 
     #collision between snake and food
     if (snake.snake[0].distance(food) < 15):
         food.refresh()
+        snake.extend()
         scoreboard.add_score()
         scoreboard.update_text()
 
@@ -40,5 +41,13 @@ while game_on:
     if (snake.snake[0].xcor() > 280 or snake.snake[0].xcor() < -280 or snake.snake[0].ycor() > 280 or snake.snake[0].ycor() < -280):
         scoreboard.game_over()
         game_on = False
+
+    #Detect collision with tail.
+    for snake_body in snake.snake:
+        if snake_body == snake.snake[0]:
+            pass
+        elif snake.snake[0].distance(snake_body) < 10:
+            game_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
